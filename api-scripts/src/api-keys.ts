ea6043006ac0759bc35e0eab49ed892aa07cac25
Benji Gogan <benji@Benjis-MacBook-Pro.local>
2023-03-17 22:05:39 +0000

    folder-rename

---
+++ api-scripts/src/api-keys.ts
@@ -0,0 +1,6 @@
+const keys = {
+	bscscan_api_key: "GF7A65XG29RJTFSTAXTCAGINV9Z1FFANYQ",
+	etherscan_api_key: "YQZ4XZ9QZ4XZ9QZ4XZ9QZ4XZ9QZ4XZ9QZ4"
+};
+
+export default keys;
--- scripts/src/api-keys.ts
+++
@@ -1,6 +0,0 @@
-const keys = {
-	bscscan_api_key: "",
-	etherscan_api_key: ""
-};
-
-export default keys;
